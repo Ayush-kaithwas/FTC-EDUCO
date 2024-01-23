33cc02a32c00a47b417e4293d9f81bc0944ee200
Omnific <tombriddle53@gmail.com>
2024-01-23 13:12:51 +0530

    Version1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Globals.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+public class Globals {
+    public static Location SIDE = Location.FAR;
+
+    public static Location ALLIANCE = Location.RED;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Location.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+
+public enum Location {
+    LEFT, CENTER, RIGHT, BLUE, RED, FAR, CLOSE
+}
